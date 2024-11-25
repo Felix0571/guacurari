ef480abfaed9d0801278f3a4af6713a4defec951
ugaisi6 <zygxqsxqy@gmail.com>
2024-11-25 11:55:22 -0300

    sad

--- sprite_2d.gd
+++ sprite_2d.gd
@@ -5,10 +5,9 @@ func sleep(seconds: int) -> Signal:
 	return get_tree().create_timer(seconds).timeout;
 
 func _ready() -> void:
-	for i in 9:
-		self.position = Vector2(10 * i, 10 * i);
-		await sleep(1);
+	pass		
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	self.position = get_global_mouse_position();
 	pass
